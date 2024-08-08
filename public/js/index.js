cf02ab1324936b4425a7a37afb54ff2cae63ecba
Angel Dimitrov <52366403+angeldimitrov94@users.noreply.github.com>
2024-08-08 04:19:16 +0000

    Cleanup of subdirectories that aren't needed for github pages

--- public/js/index.js
+++
@@ -1,9 +0,0 @@
-function markActive() {
-    const current = 0;
-    for (var i = 0; i < document.links.length; i++) {
-        if (document.links[i].href === document.URL) {
-            current = i;
-        }
-    }
-    document.links[current].className = 'current';
-}
